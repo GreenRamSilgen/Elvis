cdfc86ddefb351821bb7312e9b62bcccefcbaa4e
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-06 08:50:29 -0800

    Completed Challenge.

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "Peace"
+    @State private var sysImg = "peacesign"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            Spacer()
+            
+            VStack{
+                Image(systemName: sysImg)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                Text(message)
+                    .foregroundStyle(.purple)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+            }
+            
+            Spacer()
+            
+            HStack{
+                Button("Peace") {
+                    message = "Peace"
+                    sysImg = "peacesign"
+                }
+                Button("Love") {
+                    message = "Love"
+                    sysImg = "heart"
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    sysImg = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
