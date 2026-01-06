76b4f7d5fdb58c002755f256f34ccde8b1cb5240
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-06 08:41:38 -0800

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Kiran Shrestha on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
